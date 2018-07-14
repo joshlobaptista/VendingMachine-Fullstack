474c1637663c0ac1e74ca1bbade06a6b6098af30
Joshua Baptista <joshua.lopesbaptista@gmail.com>
2018-07-13 21:57:22 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://owner:owner1@ds133601.mlab.com:33601/vend' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
